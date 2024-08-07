a048dd3f6ad459ddbcd327e7479c2a1d633c22c3
Qiming Liu <ql2298@nyu.edu>
2024-08-07 15:44:30 -0400

    fixed the streaming and role issue left yesterday

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
+import { Readex_Pro } from "next/font/google";
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -14,18 +15,22 @@ export default function Home() {
   ]);
 
   const sendMessage = async () => {
-    setMessage("");
     setMessages((messages) => [
       ...messages,
       { role: "user", content: message },
     ]);
-    const response = await fetch("/api/chat", {
+
+    const response = fetch("/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify([...messages, { role: "user", content: message }]),
     }).then(async (res) => {
+      setMessages((messages) => [
+        ...messages,
+        { role: "assistant", content: "" },
+      ]);
       const reader = res.body.getReader();
       const decoder = new TextDecoder();
       let result = "";
@@ -36,13 +41,31 @@ export default function Home() {
         const text = decoder.decode(value || new UintBArray(), {
           stream: true,
         });
+
         setMessages((messages) => {
           let lastMessage = messages[messages.length - 1];
           let otherMessages = messages.slice(0, messages.length - 1);
 
+          // console.log(`whole message array is like: `, messages);
+
+          // messages.forEach((single_message, index) => {
+          //   console.log(`${index}th message is: `, single_message.content);
+          //   console.log(`${index}th message role is: `, single_message.role);
+          // });
+          console.log("lastMessage is: ", lastMessage.content);
+          console.log("lastMessage role is: ", lastMessage.role);
+          // otherMessages.forEach((message, index) => {
+          //   console.log(`otherMessage ${index}th is: `, message);
+          // });
+          console.log("\n");
+
           return [
             ...otherMessages,
-            { ...lastMessage, content: lastMessage.content + text },
+            {
+              ...lastMessage,
+              content: lastMessage.content + text,
+              role: "assistant",
+            },
           ];
         });
 
@@ -77,21 +100,25 @@ export default function Home() {
           flexGrow={1}
           maxHeight="100%"
           overflow="auto"
-          // spacing={3}
+          justifyContent={
+            message.role === "assistant" ? "flex.start" : "flex.end"
+          }
         >
-          {messages.map((botmessages, index) => (
+          {messages.map((message, index) => (
             <Box
               key={index}
               bgcolor={
-                botmessages.role === "assistant"
-                  ? "primary.main"
-                  : "secondary.main"
+                message.role === "assistant" ? "primary.main" : "secondary.main"
               }
               color="white"
               borderRadius={16}
               p={2}
             >
-              <Typography>{botmessages.content}</Typography>
+              {/* <Typography>
+                {index}th message is: {message.content} with role {message.role}
+              </Typography>
+              <Typography>message role is: {message.role}</Typography> */}
+              <Typography>{message.content}</Typography>
             </Box>
           ))}
         </Stack>
